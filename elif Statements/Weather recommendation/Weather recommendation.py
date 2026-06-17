ec71b97b02ab5c3ef0d03a818ac8404af60aea27
2davidjangra <2davidjangra@gmail.com>
2026-06-17 14:30:09 +0530

    Create Weather recommendation.py

---
+++ elif Statements/Weather recommendation/Weather recommendation.py
@@ -0,0 +1,10 @@
+weather = input("Enter Weather Condition: ")
+
+if weather == "Sunny":
+    print("Wear Sunglasses")
+elif weather == "Rainy":
+    print("Carry Umbrella")
+elif weather == "Cold":
+    print("Wear a Jacket")
+else:
+    print("No Specific Recommendation")
