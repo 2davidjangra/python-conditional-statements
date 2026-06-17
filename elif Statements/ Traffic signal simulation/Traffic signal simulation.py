c730d94f2aa46edd74c9869723c67d3c1cdb44d1
2davidjangra <2davidjangra@gmail.com>
2026-06-17 14:29:20 +0530

    Create  Traffic signal simulation.py

---
+++ elif Statements/ Traffic signal simulation/Traffic signal simulation.py
@@ -0,0 +1,10 @@
+signal = input("Enter Signal Color: ")
+
+if signal == "Red":
+    print("Stop")
+elif signal == "Yellow":
+    print("Wait")
+elif signal == "Green":
+    print("Go")
+else:
+    print("Invalid Signal")
