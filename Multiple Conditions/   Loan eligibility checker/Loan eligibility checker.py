39d00333d98b97fdd4cf6610314189e477d70164
2davidjangra <2davidjangra@gmail.com>
2026-06-17 14:32:36 +0530

    Create    Loan eligibility checker.py

---
+++ Multiple Conditions/   Loan eligibility checker/Loan eligibility checker.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Age: "))
+income = float(input("Enter Monthly Income: "))
+
+if age >= 21 and income >= 25000:
+    print("Eligible for Loan")
+else:
+    print("Not Eligible for Loan")
